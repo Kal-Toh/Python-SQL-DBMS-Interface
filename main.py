3fde7de38fa14372fe511aa4c3488d610559d0c7
Rory <104321806+Kal-Toh@users.noreply.github.com>
2023-02-24 12:27:55 +0000

    Added functionality to add tables from web

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+## Import modules ##
 from os import listdir
 import time
 from create_db import create_new_db
@@ -8,20 +9,32 @@ from update_data import *
 from delete_data import *
 
 #TODO - Make sure menu text inline
+#!    - Implement interpolation on add_data_web
+#!    - Add count of records added on add_data_web
+#TODO - Add ability for multi pages on add_data_web
 #TODO - Make sure sleep timing is used throughout modules 
 #TODO - Add options to return to previous menu
 #TODO - Remove symbols from pulled data
 #TODO - Display data in more table like fashion
 #TODO - Add sorting for data, maybe visualise?
 #TODO - Add option to export tables to other formats
+#TODO - Add ability to delete multiple rows based on input e.g 1, 5, 3, 9
+#TODO - Place / read databases from a database folder
+
+## Initialize connection object and database name ##
 con = None
 db_name = None
+
+
+## This function presents the user with the option to select an existing database or create a new one ##
 def start():
     print(f"""
-    Welcome to PyDBMS.
+    Welcome to PyDBMSI.
     """)
+    ## Get list of existing databases in current directory ##
     db_files = [f for f in listdir() if f.endswith(".db")]
     db_list = "\n".join([f"{i+1}. {f}" for i, f in enumerate(db_files)])
+    ## If there are existing databases, presents user with the option to select one ##
     if db_files:
         time.sleep(0.2)
         selection = input(f"""
@@ -31,30 +44,36 @@ def start():
     """)
         if selection == "y":
             time.sleep(0.2)
+            ## Present list of databases and let user select one ##
             db_selection = int(input(f"""
             Select a database:
             {db_list}
             """)) - 1
             db_name = db_files[db_selection]
+            ## Connect to selected database and return connection object and name ##
             con = sql.connect(db_name)
             return con, db_name
             
         elif selection == "n":
+            ## Create a new database and return connection object and name ##
             return create_new_db()
         else:
             time.sleep(0.2)
             print("Unrecognised selection.")
             start()
             #! Make while loop.
+    ## If there are no existing databases, prompt user to create a new one ##
     else:
         time.sleep(0.2)
         selection = input(f"""
     No databases found, would you like to create one? y/n:\n
     """)
         if selection == "y":
+            ## Create a new database and return connection object and name ##
             time.sleep(0.2)
             return create_new_db()
         elif selection == "n":
+            ## End program if user chooses not to create a new database ##
             time.sleep(0.2)
             print("Closing.")
         else:
@@ -63,7 +82,7 @@ def start():
             start()
 
 
-    
+## This function presents the user with a menu of operations to perform on the selected database ##
 def db_operations():
     time.sleep(0.2)
     print(f"""
@@ -90,14 +109,13 @@ def db_operations():
     elif selection =="4":
         time.sleep(0.02)
         delete_menu(con, cur, db_name)
-
-        
-    
     else:
-        print("Not yet implemented.")
+        print("Unrecognised selection.")
 
+## Starts program if not connected to database ##
 while con == None:
     con, db_name = start()
 cur = con.cursor()
+## If connected to database displays user operations menu ##
 while con != None:
     db_operations()
